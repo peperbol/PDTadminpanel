3259b26cabb400461bd2947aebe8c4ccfc2e5d90
Pepijn Willekens <pepijnwillekens@gmail.com>
2017-04-01 15:18:48 +0200

    fixed some frontend things

--- app/app.component.js
+++ app/app.component.js
@@ -16,7 +16,7 @@
       ],
       loadData: function(){
         var me = this;
-        this.api.getAllPrograms().then(function(d){ me.data  = d});
+        this.api.getAllPrograms().then(function(d){ me.data  = d.sort(function(a,b){return (a.program + a.graduationprogram).localeCompare(b.program + b.graduationprogram);});});
       },
       stringify: function(object){
         return JSON.stringify(object);
